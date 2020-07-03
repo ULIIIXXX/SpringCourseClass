74e3b478feba08bd21355d17399c3f28eb6ebf7d
ULIIIXXX <adolfocontreras59@gmail.com>
2020-07-02 22:17:05 -0500

    Datos de prueba insertados

--- spring-boot-data-jpa-1/src/main/java/com/bolsadeideas/springboot/app/jpa/controllers/ClienteController.java
+++ spring-boot-data-jpa-1/src/main/java/com/bolsadeideas/springboot/app/jpa/controllers/ClienteController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.app.jpa.controllers;
 
+import com.bolsadeideas.springboot.app.jpa.models.dao.ICliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClienteController {
 
+    @Autowired
+    public ICliente iCliente;
+
     @GetMapping("/listar")
     public String listarItems(Model model){
-        return "";
+        model.addAttribute("titulo","Listado de clientes");
+        model.addAttribute("clientes",iCliente.findAll());
+        return "listar";
     }
 
 }
